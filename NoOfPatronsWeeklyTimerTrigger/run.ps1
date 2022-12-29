2483b16e2b02a2b0a912dd374b78cd386790a363
Piotr <piotrosz@gmail.com>
2022-12-29 18:23:15 +0100

    test

--- NoOfPatronsWeeklyTimerTrigger/run.ps1
+++ NoOfPatronsWeeklyTimerTrigger/run.ps1
@@ -12,8 +12,8 @@ if ($Timer.IsPastDue) {
 # Write an information log with the current time.
 Write-Host "PowerShell timer trigger function ran! TIME: $currentUTCtime"
 
-
 $html = Invoke-WebRequest -Uri "https://patronite.pl/radionowyswiat"
 $reg_exp = '<span class="author__stats--number" id="stats-patrons">([\d\s]{1,8})</span>'
 $all_matches = ($html | Select-String $reg_exp -AllMatches).Matches
 Write-Host (Get-Date).ToString("yyyy-MM-dd") $all_matches.Groups[1].Value
+
